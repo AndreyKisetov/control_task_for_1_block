afaaed1280aa360f2b911a532fe9cbed17201569
Andrey Kisetov <rbcfff@yandex.ru>
2022-08-08 16:10:59 +0300

    added task solution

--- control_task/Program.cs
+++ control_task/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    // Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            string[] stringArray = new string[] { "hello", "2", "world", ":-)" };
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (3 >= stringArray[i].Length)
+                {
+                    Console.Write(stringArray[i] + "  ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
